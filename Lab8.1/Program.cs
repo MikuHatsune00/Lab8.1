3e7d3479a3c54d114997d3ebde300a2274f6dff2
Kseniia <balerinagizelle@mail.ru>
2021-11-24 16:35:51 +0300

    Add project files.

---
+++ Lab8.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\kzyukina\\OneDrive - DAE\\Документы\\06_TEST";            
+            string directoryName;
+            string directoryName1;
+            string fileName1;
+           if (Directory.Exists(path))
+            {
+                directoryName = Path.GetFileName(path);
+                Console.WriteLine("Папка {0} содержит:", directoryName);
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string s in dirs)
+                {
+                    directoryName1 = Path.GetFileName(s);
+                    Console.WriteLine(directoryName1);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы");
+                string[] fileName = Directory.GetFiles(path);
+                foreach (string s in fileName)
+                {
+                    fileName1 = Path.GetFileName(s);
+                    Console.WriteLine(fileName1);
+                }
+
+                Console.WriteLine();
+                string[] subdirectoryName = Directory.GetFiles(path,"*",SearchOption.AllDirectories);
+                Console.WriteLine(String.Join(Environment.NewLine, subdirectoryName));
+
+                Console.ReadKey();
+
+
+            }
+            
+            
+
+        }
+    }
+}
+
